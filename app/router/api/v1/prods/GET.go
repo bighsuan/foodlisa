9cee45f27cee8f5c3d698f46e2cfd976e65a4f26
bighsuan <aa860221g@gmail.com>
2021-08-05 21:06:16 +0800

    查所有商品改query條件

--- app/router/api/v1/prods/GET.go
+++ app/router/api/v1/prods/GET.go
@@ -17,21 +17,12 @@ func (obj *GET) Validate() {
 }
 
 func (obj *GET) Process() {
-	products := make([]models.Product, 0)
-	//result := obj.DB.Find(&products)
-
-	_store := obj.Ctx.Query("store")
-	storeId, _ := strconv.Atoi(_store)
-
-	// 檢查登入id和查詢商品id
+	// loginUser
 	_user := obj.Ctx.MustGet("Id").(string)
 	loginUser, _ := strconv.Atoi(_user)
 
-	if loginUser != storeId {
-		obj.Ctx.Writer.WriteHeader(403)
-		return
-	}
-	result := obj.DB.Where(&models.Product{UserId: storeId}).Find(&products)
+	products := make([]models.Product, 0)
+	result := obj.DB.Where(&models.Product{UserId: loginUser}).Find(&products)
 
 	if result.Error == nil {
 		obj.Ctx.JSON(http.StatusOK, products)
